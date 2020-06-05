e05ff9f908d87c0567198efa2b8d0824a2e21ebb
Joshua Lockerman <>
2020-06-05 14:03:45 -0400

    Add linux bindings

--- postgres-headers-rs/src/lib.rs
+++ postgres-headers-rs/src/lib.rs
@@ -1,9 +1,16 @@
 #![allow(nonstandard_style)]
 #![allow(improper_ctypes)]
 
-//TODO version selection
+// TODO version selection
+// TODO should we have a sperate linux_musl_pgN target?
+#[cfg(target_os = "linux")]
+pub use linux_glibc_pg12::*;
+
 #[cfg(target_os = "macos")]
 pub use macos_pg12::*;
+
+#[cfg(target_os = "linux")]
+mod linux_glibc_pg12;
 
 #[cfg(target_os = "macos")]
 mod macos_pg12;
