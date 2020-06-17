825a3a5a30c9e8667c97e79345111f27442bace7
Joshua Lockerman <>
2020-06-17 17:27:09 -0400

    clean up redundant code

--- timescale-extension-utils/src/lib.rs
+++ timescale-extension-utils/src/lib.rs
@@ -8,13 +8,13 @@ use std::{
             compiler_fence,
             Ordering,
         },
-        Once,
     },
 };
 
+pub use postgres_headers_rs::elog;
+
 pub use postgres_headers_rs::sys as pg_sys;
 pub mod datum;
-pub mod elog;
 pub mod palloc;
 
 //TODO postgres version
@@ -181,64 +181,6 @@ impl PGError {
         std::process::abort()
     }
 }
-
-/// Provides a barrier between Rust and Postgres' usage of the C set/longjmp
-///
-/// In the case of a longjmp being caught, this will convert that to a panic.
-/// The panic must be caught _before_ unwinding into C code.
-#[cfg(unix)]
-#[inline(never)]
-pub unsafe fn guard_pg<R, F: FnOnce() -> R>(f: F) -> R {
-    // setup the check protection
-    let original_exception_stack: *mut pg_sys::sigjmp_buf = pg_sys::PG_exception_stack;
-    let mut local_exception_stack: mem::MaybeUninit<pg_sys::sigjmp_buf> =
-        mem::MaybeUninit::uninit();
-    let jumped = pg_sys::sigsetjmp(
-        // grab a mutable reference, cast to a mutabl pointr, then case to the expected erased pointer type
-        local_exception_stack.as_mut_ptr() as *mut pg_sys::sigjmp_buf as *mut _,
-        1,
-    );
-    // now that we have the local_exception_stack, we set that for any PG longjmps...
-
-    if jumped != 0 {
-
-        pg_sys::PG_exception_stack = original_exception_stack;
-
-        // The C Panicked!, handling control to Rust Panic handler
-        compiler_fence(Ordering::SeqCst);
-
-        // if this is the first time we've caught a postgres error, set a panic
-        // hook so rust does not print an additional panic for the error.
-        static SET_PANIC_HOOK: Once = Once::new();
-        SET_PANIC_HOOK.call_once(|| {
-            let default_handler = panic::take_hook();
-            let our_handler: Box<dyn Fn(&panic::PanicInfo<'_>) + Sync + Send> =
-                Box::new(move |info| {
-                    match info.payload().downcast_ref::<PGError>() {
-                        // for pg errors postgres will handle the output
-                        Some(_) => {},
-                        // other errors we still output
-                        None => default_handler(info),
-                    }
-                });
-            panic::set_hook(our_handler);
-        });
-        panic!(PGError);
-    }
-
-    // replace the exception stack with ours to jump to the above point
-    pg_sys::PG_exception_stack = local_exception_stack.as_mut_ptr() as *mut _;
-
-    // enforce that the setjmp is not reordered, though that's probably unlikely...
-    compiler_fence(Ordering::SeqCst);
-    let result = f();
-
-    compiler_fence(Ordering::SeqCst);
-    pg_sys::PG_exception_stack = original_exception_stack;
-
-    result
-}
-
 
 /// try executing a closure, running `pre_re_throw` in the event that the
 /// closure throws a postgres exception.
